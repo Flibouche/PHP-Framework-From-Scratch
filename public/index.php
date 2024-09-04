43389bb8382c2fa6999138a7c777152e75d13030
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-04 15:37:29 +0200

    Add routing to the application.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,26 @@
 <?php
 
-echo "Hello World!";
+session_start();
 
-?>
+class App
+{
+    private function splitURL()
+    {
+        $URL = $_GET['url'] ?? 'home';
+        $URL = explode("/", $URL);
+        return $URL;
+    }
+
+    private function loadController()
+    {
+        $URL = splitURL();
+
+        $filename = "../app/controllers/" . ucfirst($URL[0]) . ".php";
+        if (file_exists($filename)) {
+            require $filename;
+        } else {
+            $filename = "../app/controllers/_404.php";
+            require $filename;
+        }
+    }
+}
