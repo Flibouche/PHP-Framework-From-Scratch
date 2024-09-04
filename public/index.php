8fb992966298cb50949c1f4a495bf9493d43172a
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-04 15:47:06 +0200

    Add core files.

--- public/index.php
+++ public/index.php
@@ -2,25 +2,7 @@
 
 session_start();
 
-class App
-{
-    private function splitURL()
-    {
-        $URL = $_GET['url'] ?? 'home';
-        $URL = explode("/", $URL);
-        return $URL;
-    }
+require "../app/core/init.php";
 
-    private function loadController()
-    {
-        $URL = splitURL();
-
-        $filename = "../app/controllers/" . ucfirst($URL[0]) . ".php";
-        if (file_exists($filename)) {
-            require $filename;
-        } else {
-            $filename = "../app/controllers/_404.php";
-            require $filename;
-        }
-    }
-}
+$app = new App;
+$app->loadController();
