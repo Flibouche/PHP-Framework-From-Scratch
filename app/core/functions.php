a1463fe937255e98a6dbe4f8b08e6a79a1587669
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-10 10:29:57 +0200

    Check PHP extensions.

--- app/core/functions.php
+++ app/core/functions.php
@@ -1,6 +1,36 @@
 <?php
 
-defined('ROOTPATH') OR exit('Access Denied');
+defined('ROOTPATH') or exit('Access Denied');
+
+check_extensions();
+function check_extensions()
+{
+    $required_extensions = [
+        'gd',
+        'mysqli',
+        'curl',
+        'fileinfo',
+        'intl',
+        'mbstring',
+        'exif',
+        'openssl',
+        'pdo_mysql',
+        'xsl',
+    ];
+
+    $not_loaded = [];
+
+    foreach ($required_extensions as $ext) {
+        if (!extension_loaded($ext)) {
+            $not_loaded[] = $ext;
+        }
+    }
+
+    if (!empty($not_loaded)) {
+        show("Please load the following extensions in your php.ini file: <br>" . implode("<br>", $not_loaded));
+        die;
+    }
+}
 
 function show($stuff)
 {
