3a713080849bda0c87cf8181b543fe554226776b
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-10 15:25:38 +0200

    Add Image class.

---
+++ app/models/Image.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Image manipulation class
+ */
+
+namespace Model;
+
+defined('ROOTPATH') or exit('Access Denied');
+
+class Image
+{
+
+    // Fonction qui permet de redimensionner une image
+    public function resize($filename, $max_size = 700)
+    {
+
+        // Check quel type de fichier c'est
+        $type = mime_content_type($filename);
+
+        if (file_exists($filename)) {
+            switch ($type) {
+
+                case 'image/png':
+                    $image = imagecreatefrompng($filename);
+                    break;
+
+                case 'image/gif':
+                    $image = imagecreatefromgif($filename);
+                    break;
+
+                case 'image/jpeg':
+                    $image = imagecreatefromjpeg($filename);
+                    break;
+
+                case 'image/webp':
+                    $image = imagecreatefromwebp($filename);
+                    break;
+
+                default:
+                    return $filename;
+                    break;
+            }
+
+            // Une fois l'image chargée, on peut la redimensionner
+            $src_w = imagesx($image);
+            $src_h = imagesy($image);
+
+            if ($src_w > $src_h) {
+                // Réduit la taille max si l'image est plus petite si elle est plus large
+                if ($src_w < $max_size) {
+                    $max_size = $src_w;
+                }
+
+                $dst_w = $max_size;
+                $dst_h = ($src_h / $src_w) * $max_size;
+            } else {
+                // Réduit la taille max si l'image est plus petite si elle est plus haute
+                if ($src_h < $max_size) {
+                    $max_size = $src_h;
+                }
+
+                $dst_w = ($src_w / $src_h) * $max_size;
+                $dst_h = $max_size;
+            }
+
+            $dst_w = round($dst_w);
+            $dst_h = round($dst_h);
+
+            $dst_image = imagecreatetruecolor($dst_w, $dst_h);
+
+            if ($type == 'image/png') {
+                imagealphablending($dst_image, false); // Permet de garder la transparence
+                imagesavealpha($dst_image, true);
+            }
+
+            imagecopyresampled($dst_image, $image, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
+
+            imagedestroy($image);
+
+            switch ($type) {
+
+                case 'image/png':
+                    imagepng($dst_image, $filename, 8);
+                    break;
+
+                case 'image/gif':
+                    imagegif($dst_image, $filename);
+                    break;
+
+                case 'image/jpeg':
+                    imagejpeg($dst_image, $filename, 90);
+                    break;
+
+                case 'image/webp':
+                    imagewebp($dst_image, $filename, 90);
+                    break;
+
+                default:
+                    imagejpeg($dst_image, $filename, 90);
+                    break;
+            }
+
+            imagedestroy($dst_image);
+        }
+
+        return $filename;
+    }
+}
