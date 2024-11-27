29bf2800a5da5e03c3b29945e27a29e881527751
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-27 16:35:26 +0100

    👌 IMPROVE: Clean home controller and view to remove image handling and thumbnail generation.

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -13,12 +13,6 @@ class Home
 
     public function index()
     {
-        $file = 'pexels-felicity-tai-7966072.jpg';
-
-        $data['file'] = $file;
-        $image = new \Model\Image();
-        $image->getThumbnail($file, 200, 1000);
-        $data['thumbnail'] = $image->getThumbnail($file, 80, 80);
-        $this->view('home', $data);
+        $this->view('home');
     }
 }
